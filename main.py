e444286674ec333f0a0832e506996cd49bfb72a9
s22714 <s22714@pjwstk.edu.pl>
2023-03-10 17:03:25 +0100

    test run of project

---
+++ main.py
@@ -0,0 +1,10 @@
+import webbrowser
+import requests
+pageurl = input("Podaj link: ")
+date = input("Podaj date w formacie YYYYMMDD: ")
+
+url ="http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
+response = requests.get(url)
+d = response.json()
+page = d["archived_snapshots"]["closest"]["url"]
+webbrowser.open(url)
